dc67acc4407cbbf4dfccaea9cc1eeeb96990f77a
easylearningbd <elebdsoft@gmail.com>
2025-10-11 04:19:57 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Middleware\IsAdmin;
+use App\Http\Middleware\IsUser;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//// All User Protected Routes
+
+Route::middleware(['auth', IsUser::class])->group(function(){
+
+ Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
+});
+
+
+//// End All User Protected Routes
+
+
+
+//// All Admin Protected Routes
+
+Route::middleware(['auth', IsAdmin::class])->group(function(){
+
+  Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+
+
+});
+
+//// End All Admin Protected Routes
+
+
+
+
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
